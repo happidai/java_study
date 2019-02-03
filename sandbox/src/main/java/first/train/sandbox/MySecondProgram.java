5d26dafecb48c95b6883d248abfadf87132a8cdc
happidai <42470198+happidai@users.noreply.github.com>
2019-02-03 22:10:02 +0300

    try to do the 2 task

---
+++ sandbox/src/main/java/first/train/sandbox/MySecondProgram.java
@@ -0,0 +1,13 @@
+package first.train.sandbox;
+
+public class MySecondProgram {
+
+    public static void main (String[] args) {
+
+Point mypoint = new Point(4.0, 9.0);
+        System.out.println(mypoint.distance());
+
+    }
+
+
+}
---
+++ sandbox/src/main/java/first/train/sandbox/Point.java
@@ -0,0 +1,22 @@
+package first.train.sandbox;
+
+import static jdk.nashorn.internal.objects.NativeMath.sqrt;
+
+public class Point {
+
+   public double p1;
+   public double p2;
+
+   public Point (double p1, double p2){
+       this.p1 = p1;
+       this.p2 = p2;
+     
+
+   }
+
+   public static double distance(Point p1, Point p2){
+       return (double) Math.sqrt();
+   }
+
+
+}
