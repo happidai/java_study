91631ea676a4ecb9ba6522b70f31e53551c9b5ad
happidai <42470198+happidai@users.noreply.github.com>
2019-02-03 16:48:22 +0300

    не хочу отменять изменения

--- sandbox/src/main/java/first/train/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/first/train/sandbox/MyFirstProgram.java
@@ -5,6 +5,20 @@ public class MyFirstProgram {
 	public static void main (String[] args){
 
 System.out.println("Hello world");
+
+		System.out.println(2 +2);
+		System.out.println(1/2);
+		System.out.println("float: "+ 1.0/2);
+		System.out.println("конкатенация: "+ "2"+"2");
+		System.out.println("число преобразовывается в строку: " + 2 + "2");
+		System.out.println("числа преобразовавыютя в строку. чтобы получить 4, нужны скобки: "+"2+2="+2+2);
+		System.out.println("2+2= "+ (2+2));
+
+
+
+
+
+
 	}
 
 }
