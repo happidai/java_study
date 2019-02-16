c142857ac149cbdf4d7837d44d9027f018fc7a62
happidai <42470198+happidai@users.noreply.github.com>
2019-02-16 12:58:30 +0300

    if - else EquationTests

---
+++ sandbox/src/test/java/first/train/sandbox/EquationTests.java
@@ -0,0 +1,62 @@
+package first.train.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+
+    @Test
+    public void test0() {
+
+        Equation e = new Equation(1, 1, 1);
+        Assert.assertEquals(e.rootNumer(), 0);
+
+
+    }
+
+    @Test
+    public void test1() {
+
+        Equation e = new Equation(1, 2, 1);
+        Assert.assertEquals(e.rootNumer(), 1);
+
+
+    }
+
+    @Test
+    public void test2() {
+
+        Equation e = new Equation(1, 5, 6);
+        Assert.assertEquals(e.rootNumer(), 2);
+
+
+    }
+
+    @Test
+    public void testLinear() {
+
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNumer(), 1);
+
+
+    }
+
+    @Test
+    public void testConstant() {
+
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNumer(), 0);
+
+
+    }
+
+    @Test
+    public void testZero() {
+
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNumer(), -1);
+
+
+    }
+
+}
