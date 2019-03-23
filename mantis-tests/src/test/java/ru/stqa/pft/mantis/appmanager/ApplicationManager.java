8b5bcc0da7c527288e2e89067b05cad6d2eebc25
happidai <42470198+happidai@users.noreply.github.com>
2019-03-23 18:16:36 +0300

    Замена файла для отключения капчи по фтп

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
 
 
     public ApplicationManager(String browser)  {
@@ -52,6 +53,13 @@ public class ApplicationManager {
     public String getProperty(String key) {
         return properties.getProperty(key);
 
+    }
+
+    public FtpHelper ftp(){
+        if(ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
     }
 
     public RegistrationHelper registration() {
