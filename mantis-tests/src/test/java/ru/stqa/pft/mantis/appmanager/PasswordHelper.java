9ee878b27e0c0b3544585e4ac9a14177e1143c35
happidai <42470198+happidai@users.noreply.github.com>
2019-04-04 22:14:57 +0300

    Задание 18, исправленный PasswordHelper

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PasswordHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/PasswordHelper.java
@@ -8,46 +8,30 @@ public class PasswordHelper extends HelperBase {
             super(app);
         }
 
-        public void login() {
-            String admin = app.getProperty("web.adminLogin");
-            String adminPw = app.getProperty("web.adminPassword");
-            type(By.name("username"), admin);
-            click(By.cssSelector("input[value='Login']"));
-            type(By.name("password"), adminPw);
-            click(By.cssSelector("input[value='Login']"));
-        }
-
-        public void goToManagePage() {
-            wd.findElement(By.linkText("Manage")).click();
-        }
-
-        public void goToManageUsersPage() {
-            wd.findElement(By.linkText("Manage Users")).click();
-        }
-
-        public void chooseUser() {
-            //выбирается заданный юзер, который уже создан
-            wd.findElement(By.cssSelector("a[href*='manage_user_edit_page.php?user_id=3']")).click();
-        }
-
-        public void resetPassword() {
-            wd.findElement(By.cssSelector("input[value='Reset Password']")).click();
-        }
-
-        public String getUserName() {
-            String username = wd.findElement(By.cssSelector("input[name='username']")).getAttribute("value");
-            return username;
-        }
-
-        public String getMail() {
-            String email = wd.findElement(By.cssSelector("input[name='email']")).getAttribute("value");
-            return email;
-        }
-
-        public void changePasswordFinish(String resetPasswordLink, String newPassword) {
-            wd.get(resetPasswordLink);
-            type(By.name("password"), newPassword);
-            type(By.name("password_confirm"), newPassword);
-            click(By.cssSelector("input[value='Update User']"));
-        }
+    public void login() {
+        String admin = app.getProperty("web.adminLogin");
+        String adminPw = app.getProperty("web.adminPassword");
+        type(By.name("username"), admin);
+        click(By.cssSelector("input[value='Login']"));
+        type(By.name("password"), adminPw);
+        click(By.cssSelector("input[value='Login']"));
     }
+
+    public void goToManagePage() {
+        wd.findElement(By.linkText("Manage")).click();
+    }
+
+    public void goToManageUsersPage() {
+        wd.findElement(By.linkText("Manage Users")).click();
+    }
+
+    public void chooseUser(String username) {
+        wd.findElement(By.linkText(username)).click();
+    }
+
+    public void resetPassword() {
+        wd.findElement(By.cssSelector("input[value='Reset Password']")).click();
+    }
+
+    
+}
