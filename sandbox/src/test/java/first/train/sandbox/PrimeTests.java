476a1d64ece7168acd886c81834b7da37cb9e874
happidai <42470198+happidai@users.noreply.github.com>
2019-04-06 19:01:41 +0300

    reports

--- sandbox/src/test/java/first/train/sandbox/PrimeTests.java
+++ sandbox/src/test/java/first/train/sandbox/PrimeTests.java
@@ -1,35 +1,24 @@
 package first.train.sandbox;
 
+
 import org.testng.Assert;
-import org.testng.annotations.Test;
+        import org.testng.annotations.Test;
 
 public class PrimeTests {
-
-    @Test
-
-    public void testPrime() {
-
+    @Test(enabled = false)
+    public void testPrimes() {
         Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
-
     }
 
-    @Test
-
+    @Test(enabled = false)
     public void testPrimeLong() {
         long n = Integer.MAX_VALUE;
-
         Assert.assertTrue(Primes.isPrime(n));
-
     }
 
-
-
-    @Test
-
-    public  void testNonPrime(){
-
+    @Test(enabled = false)
+    public void testNonPrimes() {
         Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+
     }
-
-
 }
